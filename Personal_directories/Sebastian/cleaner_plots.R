ff9f7cef36e7e43a1a4d1e3569f872928ff26159
sedavbotero <sdboterol@protonmail.com>
2024-11-06 17:14:07 +0100

    coś

--- Personal_directories/Sebastian/cleaner_plots.R
+++ Personal_directories/Sebastian/cleaner_plots.R
@@ -3,8 +3,9 @@ library(ggplot2)
 library(dplyr)
 
 un_wpp <- read.csv("../Mikolaj/WPP2024_Demographic_Indicators_Medium.csv.gz")
-unigme_total <- read_xlsx('../Mikolaj/UNIGME-2023-Total-U5MR-IMR-and-NMR-database.xlsx',3)
-unigme_wealth <- read_xlsx('../Mikolaj/UNIGME-2023-Wealth-quintile-U5MR-database.xlsx',3)
+unigme_total <- read.csv('UNIGME-2023-Total-U5MR-database.csv')
+unigme_wealth <- read.csv('UNIGME-2023-Wealth-quintile-U5MR-database.csv',
+                          sep = ";", dec = ",")
 causes_death <- read.csv('../Mikolaj/CA-CODE-2024-Under5.csv')
 
 world <- map_data("world")
@@ -39,28 +40,28 @@ country_name_replacements <- matrix(
      "Eswatini", "Swaziland",
      "State of Palestine", "Palestine"), ncol = 2, byrow = TRUE)
 
-un_wpp |> 
-  mutate(Location = fix_names(Location, country_name_replacements)) |>
-  filter(Location %in% world$region, Time == '1950')|>
-  select(region = Location, u5mr=Q5)|>
-  right_join(world) |>
-  ggplot(mapping = aes(x = long, y = lat, group = group)) +
-  coord_fixed(1.3)+
-  geom_polygon(aes(fill = u5mr)) +
-  scale_fill_distiller(palette ="RdBu", direction = -1) +
-  labs(fill = "Under 5 mortality rate")
+# un_wpp |> 
+#   mutate(Location = fix_names(Location, country_name_replacements)) |>
+#   filter(Location %in% world$region, Time == '1950')|>
+#   select(region = Location, u5mr=Q5)|>
+#   right_join(world) |>
+#   ggplot(mapping = aes(x = long, y = lat, group = group)) +
+#   coord_fixed(1.3)+
+#   geom_polygon(aes(fill = u5mr)) +
+#   scale_fill_distiller(palette ="RdBu", direction = -1) +
+#   labs(fill = "Under 5 mortality rate")
 
-un_wpp |> 
-  mutate(Location = fix_names(Location, country_name_replacements)) |>
-  filter(Location %in% world$region, Time == '2023')|>
-  select(region = Location, u5mr=Q5)|>
-  right_join(world) |>
-  ggplot(mapping = aes(x = long, y = lat, group = group)) +
-  coord_fixed(1.3)+
-  geom_polygon(aes(fill = u5mr)) +
-  scale_fill_distiller(palette ="RdBu", direction = -1) +
-  labs(fill = "Under 5 mortality rate")
-
+# un_wpp |> 
+#   mutate(Location = fix_names(Location, country_name_replacements)) |>
+#   filter(Location %in% world$region, Time == '2023')|>
+#   select(region = Location, u5mr=Q5)|>
+#   right_join(world) |>
+#   ggplot(mapping = aes(x = long, y = lat, group = group)) +
+#   coord_fixed(1.3)+
+#   geom_polygon(aes(fill = u5mr)) +
+#   scale_fill_distiller(palette ="RdBu", direction = -1) +
+#   labs(fill = "Under 5 mortality rate")
+# 
 
 un_wpp |> 
   mutate(Location = fix_names(Location, country_name_replacements)) |>
@@ -71,6 +72,7 @@ un_wpp |>
   coord_fixed(1.3) +
   # coord_map() +
   geom_polygon(aes(fill = u5mr), colour = "#333333", size = 0.2) +
+  # scale_fill_distiller(palette = "PuBuGn", direction = 1) +
   # scale_fill_distiller(palette = "Reds", direction = 1) +
   scale_fill_gradient2(low = "#ccffdd",
                        mid = "#ccaa44",
@@ -83,3 +85,38 @@ un_wpp |>
   # theme(panel.background = element_rect(fill = "#cccccc"),
   #       panel.grid.major = element_line(colour = "grey"),
   #       panel.grid.minor = element_line(colour = "grey"))
+# 
+# x <- un_wpp |>
+#   filter(Time == 1950) |>
+#   select(Location, u5mr1950 = Q5)
+# 
+# un_wpp |>
+#   filter(Time == 2023) |>
+#   select(Location, u5mr2023 = Q5) |>
+#   left_join(x) |>
+#   filter(u5mr2023 > u5mr1950) |>
+#   View()
+
+
+#Demographic and Health Survey
+
+unigme_wealth_processed <- unigme_wealth |>
+  rename(survey_name = Series.Name,
+         country = Country.Name,
+         year = Series.Year,
+         reference_date = Reference.Date,
+         wealth_quintile = Wealth.Quintile,
+         u5mr = Estimates)
+
+uw_dem <- unigme_wealth_processed |> 
+  filter(survey_name == 'Demographic and Health Survey') |> 
+  group_by(country) |> 
+  slice_max(n= 1,order_by=tibble(year,reference_date))
+
+
+# Moim zdaniem ten wykres ma lepsze dane
+ggplot(uw_dem, aes(factor(wealth_quintile), u5mr)) + # tylko Afryka: |> filter(country %in% am_countries$Country_ISO)
+  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) 
+
+
+  
